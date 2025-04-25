66e6cdf6a08ab233c65c69239ca1dafcb5cc14a1
deepak belamana <saideepak627@gmail.com>
2025-04-25 06:44:25 +0530

    -generic db propeties added -open endpoint configured to check the status

--- src/main/java/com/semicolon/RepSetGoApi/config/security/WebSecurity.java
+++ src/main/java/com/semicolon/RepSetGoApi/config/security/WebSecurity.java
@@ -52,6 +52,7 @@ public class WebSecurity {
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers(HttpMethod.POST, "/rep-set-go/users").permitAll()
                 .requestMatchers("/rep-set-go/users/login").permitAll()
+                .requestMatchers("/rep-set-go/staus").permitAll()
                 .requestMatchers("/rep-set-go/**").authenticated()
                 .anyRequest().authenticated()
             )
